98c782d77629345df815e0df9239a5aff9cc53ee
zhengsq23 <2270833979@qq.com>
2024-04-19 22:56:37 +0800

    create restaurant form

---
+++ src/pages/ManageRestaurantPage.tsx
@@ -0,0 +1,10 @@
+  import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
+  
+  const ManageRestaurantPage = () => {  
+    return (
+        <ManageRestaurantForm      />
+    );
+  };
+  
+  export default ManageRestaurantPage;
+  
