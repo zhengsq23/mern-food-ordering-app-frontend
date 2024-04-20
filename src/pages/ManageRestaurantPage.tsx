25c0dc1afecb04f0391163797a17df998117a8ae
zhengsq23 <2270833979@qq.com>
2024-04-20 11:55:02 +0800

    Update my restaurant Request

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,18 +1,24 @@
 import {
   useCreateMyRestaurant,
   useGetMyRestaurant,
+  useUpdateMyRestaurant,
 } from "@/api/MyRestaurantApi";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantPage = () => {
-  const { createRestaurant, isLoading: isCreateLoading } = useCreateMyRestaurant();
+  const { createRestaurant, isLoading: isCreateLoading } =
+    useCreateMyRestaurant();
   const { restaurant } = useGetMyRestaurant();
+  const { updateRestaurant, isLoading: isUpdateLoading } =
+    useUpdateMyRestaurant();  
+  
+  const isEditing = !!restaurant;
   return (
     <ManageRestaurantForm
-      restaurant={restaurant}
-      onSave={createRestaurant}
-      isLoading={isCreateLoading}
-    />
+    restaurant={restaurant}
+    onSave={isEditing ? updateRestaurant : createRestaurant}
+    isLoading={isCreateLoading || isUpdateLoading}
+/>
   );
 };
 
