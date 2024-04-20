78224f94f0076b0ce68e55d1c22ffde540282dbb
zhengsq23 <2270833979@qq.com>
2024-04-20 14:33:54 +0800

    search API request

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,10 +1,27 @@
+import { useSearchRestaurants } from "@/api/RestaurantApi";
 import { useParams } from "react-router-dom";
+
+export type SearchState = {
+  searchQuery: string;
+  page: number;
+  selectedCuisines: string[];
+  sortOption: string;
+};
 
 const SearchPage = () => {
   const { city } = useParams();
-
+  const { results } = useSearchRestaurants(city);
+  console.log(results);
   return (
-    <span>search {city}</span>
+    <span>
+      search {city}
+      <span>{
+      
+      results?.data.map((restaurant) => <span>
+       found {restaurant.restaurantName},{restaurant.city}
+      </span>)}</span>
+      
+    </span>
   );
 };
 
