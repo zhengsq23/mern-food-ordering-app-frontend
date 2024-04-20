71c167fb4ffba968c61b2b090c47f7c8e960f2f2
zhengsq23 <2270833979@qq.com>
2024-04-20 17:31:25 +0800

    Detail page and menu items

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -6,6 +6,7 @@ import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 import SearchPage from "./pages/SearchPage";
+import DetailPage from "./pages/DetailPage";
 
 const AppRoutes = () => {
   return (
@@ -27,6 +28,15 @@ const AppRoutes = () => {
           </Layout>
         }
       />
+      <Route
+        path="/detail/:restaurantId"
+        element={
+          <Layout showHero={false}>
+            <DetailPage />
+          </Layout>
+        }
+      />
+
       <Route element={<ProtectedRoute />}>
         <Route
           path="/user-profile"
---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,21 @@
+import { MenuItem as  MenuItems } from "../types";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+
+type Props = {
+  menuItem: MenuItems;
+};
+
+const MenuItem = ({ menuItem }: Props) => {
+  return (
+    <Card className="cursor-pointer" >
+      <CardHeader>
+        <CardTitle>{menuItem.name}</CardTitle>
+      </CardHeader>
+      <CardContent className="font-bold">
+        £{(menuItem.price / 100).toFixed(2)}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default MenuItem;
